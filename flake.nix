0a7652850257dbc63102ccf835e64b9ca424b394
ryunix <ryunix.net@gmail.com>
2026-04-01 10:36:50 +0900

    build: switch nixpkgs to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   inputs = {
-    # This is pointing to an unstable release.
-    # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
-    # i.e. nixos-24.11
-    # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
   outputs = inputs@{ self, nixpkgs, ... }: {
     # NOTE: 'nixos' is the default hostname
