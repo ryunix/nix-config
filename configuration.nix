34a8f71e60b2eac8a86cb0d59938c7d57f34a794
ryunix <ryunix.net@gmail.com>
2026-04-01 11:52:03 +0900

    style: apply nixfmt-tree formatting

--- configuration.nix
+++ configuration.nix
@@ -2,15 +2,23 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -34,9 +42,6 @@
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
-
-
-  
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -120,4 +125,3 @@
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
-
--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations.archetype = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    {
+      nixosConfigurations.archetype = nixpkgs.lib.nixosSystem {
+        modules = [ ./configuration.nix ];
+      };
+
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
     };
-
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
-  };
 }
-
