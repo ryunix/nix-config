07b99a35e9e9c81f024d4f61c931feab6f469b9b
ryunix <ryunix.net@gmail.com>
2026-04-01 14:12:33 +0900

    build: add disko input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    disko = {
+      url = "github:nix-community/disko/v1.13.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     inputs@{ self, nixpkgs, ... }:
