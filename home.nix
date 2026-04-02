eddeb082fa817d66a3692cda836630f125b8f0a0
ryunix <ryunix.net@gmail.com>
2026-04-02 17:17:31 +0900

    feat: add desktop utilities

--- home.nix
+++ home.nix
@@ -23,6 +23,11 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    pkgs.keepassxc
+    pkgs.scrot
+    pkgs.sxiv
+    pkgs.xclip
+
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
