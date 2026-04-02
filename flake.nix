ed4eff365036d627aece04e6412e9fb312b415d7
ryunix <ryunix.net@gmail.com>
2026-04-02 10:15:41 +0900

    build: add home-manager input

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     disko = {
       url = "github:nix-community/disko/v1.13.0";
       inputs.nixpkgs.follows = "nixpkgs";
