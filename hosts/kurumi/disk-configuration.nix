b5e0ef4c3f6d911867d54193d455a05f8a85b510
ryunix <ryunix.net@gmail.com>
2026-04-03 11:21:31 +0900

    chore(kurumi): initialize configuration from archetype

---
+++ hosts/kurumi/disk-configuration.nix
@@ -0,0 +1,47 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "fmask=0077"
+                  "dmask=0077"
+                ];
+              };
+            };
+            swap = {
+              size = "4G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
